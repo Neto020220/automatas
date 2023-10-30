37bf7d07090eb4db149bc0152e0b13ae34778e0a
Neto020220 <129471063+Neto020220@users.noreply.github.com>
2023-10-30 14:38:08 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+def procesarCadena(entrada):
+    # Implement your logic to process the input string
+    # For demonstration purposes, let's assume the processing is converting the string to uppercase
+    cadenaProcesada = entrada.upper()
+    return cadenaProcesada
+
+def checkSintaxisCorrect(entrada):
+    if entrada != "":
+        return True
+    return "Error de sintaxis"
+
+
+@app.route("/", methods=["GET", "POST"])
+def homepage():
+    cadenaProcesada = None
+
+    if request.method == "POST":
+        entrada = request.form.get("entrada")
+        cadenaProcesada = procesarCadena(entrada)
+
+    return render_template("index.html", title="Lenguajes y Automatas II", cadenaProcesada=cadenaProcesada)
+
+@app.route("/analisis_sintactico.html")
+def analisisSintactico():
+    return render_template("analisis_sintactico.html")
+
+@app.route("/analisis_lexico.html")
+def analisisLexico():
+    return render_template("analisis_lexico.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
